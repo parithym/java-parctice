3c62474b173c4fd931cca8c547cecf1e5c36c473
parithym <parithym05@gmail.com>
2025-09-10 19:53:40 +0530

    Create TemperatureConversion.java
    This program gets input from the user in double datatype  as 
    Celsius to convert it into Fahrenheit. The  temperature conversion is done here.

---
+++ TemperatureConversion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+        System.out.println("Give celsius: ");
+        double celsius = s.nextDouble();
+        double fahrenheit = (celsius * 9.0/5.0 )+32;
+        System.out.println("Fahrenheit: "+fahrenheit);
+        
+        
+    }
+}
+
+
+
+Give celsius: 
+17.23
+Fahrenheit: 63.013999999999996
+
+=== Code Execution Successful ===
