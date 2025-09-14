fa42f946674352ed75bee586a7c134dfba072d40
parithym <parithym05@gmail.com>
2025-09-14 20:47:02 +0530

    Create EvenNoWhile.java
    This program prints Even number from 1-50 using while loops.

---
+++ EvenNoWhile.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Even numbers");
+      int i = 1;
+      while(i <= 50){
+        if(i % 2 == 0){
+          System.out.println(i);
+        }
+        i++;
+      }
+  }
+}
+
+
+OUTPUT:
+Output:
+
+Even numbers
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
