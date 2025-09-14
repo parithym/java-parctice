8625410a61e8ce5ad9cb642d9ad2b675f508e42b
parithym <parithym05@gmail.com>
2025-09-14 20:42:26 +0530

    Create EvenNo.java
    This program prints even number from 1-50 using for loop.

---
+++ EvenNo.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Even numbers");
+      for(int i = 1 ; i <= 50 ; i++ ) {
+        if(i % 2 == 0) {
+          System.out.println(i);
+        }
+      }
+  }
+}
+
+
+
+
+OUTPUT:
+Even numbers
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
